4cffe104fed234b4fa2b213cde416325b9cdfbb8
ElDeBondislav <grunt6519@gmail.com>
2022-08-15 10:18:19 +0300

    message

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ import time
 from django.views.decorators.csrf import csrf_exempt
 from django_user_agents.utils import get_user_agent
 from .content import *
-from telegrambot import getBot
+from .telegrambot import getBot
 from telegram import *
 from telegram.ext import *
 
